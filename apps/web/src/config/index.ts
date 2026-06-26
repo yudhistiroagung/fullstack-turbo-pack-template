f38d0eeddb38b12ba33d1b024aa284b7e7cde397
yudhistiroagung <yudhistiro.agung@gmail.com>
2026-06-26 18:52:26 +0700

    safely get env variable

---
+++ apps/web/src/config/index.ts
@@ -0,0 +1,25 @@
+const ENV = import.meta.env as Record<string, unknown>;
+
+const getString = (key: string) => {
+    const value = ENV[key];
+    if (typeof value === 'string') {
+        return value
+    }
+    
+    throw new Error(`VITE_${key} is not a string`);
+};
+
+const getInt = (key: string) => {
+    const value = ENV[key];
+    if (typeof value === 'number') {
+        return value
+    }
+    
+    throw new Error(`VITE_${key} is not a number`);
+};
+
+const config = {
+  appTitle: getString('VITE_APP_TITLE'),
+  appVersion: getInt('VITE_APP_VERSION'),
+  apiUrl: getString('VITE_API_URL'),
+}
